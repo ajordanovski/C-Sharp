8f9c010c7cb6b3557c356df6326c250791bcd4df
Angel Jordanovski <76494539+ajordanovski@users.noreply.github.com>
2021-03-31 00:53:38 +0200

    Homework.Class03

---
+++ 1. C# Basic/Class 03/Class03.Homework.StudentGroup/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Class03.Homework.StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //## Task 2
+            //*Make a new console application called StudentGroup
+            //* Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names. 
+            //* Get a number from the console between 1 and 2 and print the students from that group in the console.
+            //* Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+            //* Test Data:
+            //*Enter student group: (there are 1 and 2 )
+            //    *1
+            //* Expected Output:
+            //*The Students in G1 are: 
+            //  *Zdravko
+            //  * Petko
+            //  * Stanko
+            //  * Branko
+            //  * Trajko
+
+            string[] studentsG1 = { "Hobbit", "Elf", "Dwarf", "Ork", "Smigol" };
+            string[] studentsG2 = { "Jack", "Neo", "Optimus", "Joker", "Ironman" };
+
+            Console.WriteLine("input number 1 (students from group 1) or number 2 (students from group 2)");
+            int input = int.Parse(Console.ReadLine());
+            
+            switch(input)
+            {
+                case 1:
+                    for (int i = 0; i < studentsG1.Length; i++)
+                    {
+                        Console.WriteLine(studentsG1[i]);
+                    };
+                    break;
+                case 2:
+                    for (int i = 0; i < studentsG2.Length; i++)
+                    {
+                        Console.WriteLine(studentsG2[i]);
+                    };
+                    break;
+                default:
+                    Console.WriteLine("invalid number: input 1 or 2");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1. C# Basic/Class 03/Class03.Homework.SumOfEven/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Class03.Homework.SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //## Task 1
+            //Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+            //*Test Data:
+            //*Enter integer no.1:
+            //    *4
+            //  * Enter integer no.1:
+            //    *3
+            //  * Enter integer no.1:
+            //    *7
+            //  * Enter integer no.1:
+            //    *3
+            //  * Enter integer no.1:
+            //    *2
+            //  * Enter integer no.1:
+            //    *8
+            //* Expected Output:
+            //*The result is: 14
+
+
+            int[] emptyIntArray = new int[6];
+
+            int sumOfEven = 0;
+
+            for(int i = 0; i < emptyIntArray.Length; i++)
+            {
+                Console.WriteLine("Input number in Array");
+                string input = Console.ReadLine();
+                bool isValidInput = int.TryParse(input, out int userInput);
+                emptyIntArray[i] = userInput;
+                //emptyIntArray[i] = int.Parse(Console.ReadLine());
+                if (emptyIntArray[i] % 2 == 0)
+                {
+                    sumOfEven += emptyIntArray[i];
+                }
+            }
+
+            Console.WriteLine("The sum of the even numbers from the array is " + sumOfEven);
+
+            Console.ReadLine();
+        }
+    }
+}
